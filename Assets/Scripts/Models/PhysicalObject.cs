7508f0ef11f24fc740ef54d5e099ed888d4bf458
Даниил Радченко <37844948+danila032040@users.noreply.github.com>
2020-12-09 19:46:03 +0300

    PhysicalObject externalForce commit
    Also added folder for future prefabs of blocks, changed camera projection from perspective to ortographic.

--- Assets/Scripts/Models/PhysicalObject.cs
+++ Assets/Scripts/Models/PhysicalObject.cs
@@ -8,11 +8,11 @@ public class PhysicalObject : MonoBehaviour
     [SerializeField]
     private float weight = 1f;
 
-    private Vector3 force;
+    private Vector3 externalforce;
     private Vector3 speed;
 
     public Vector3 Acceleration => Force / weight;
-    public Vector3 Force => force + Vector3.down * gravity * weight;
+    public Vector3 Force => externalforce + Vector3.down * gravity * weight;
     public Vector3 Speed => speed;
 
     void Update()
@@ -30,8 +30,8 @@ public class PhysicalObject : MonoBehaviour
     {
         switch(mode)
         {
-            case ForceMode.Acceleration: this.force += force * weight; break;
-            case ForceMode.Force: this.force += force;  break;
+            case ForceMode.Acceleration: this.externalforce += force * weight; break;
+            case ForceMode.Force: this.externalforce += force;  break;
             case ForceMode.Impulse: this.speed += force / weight; break;
             case ForceMode.VelocityChange: this.speed += force; break;
         }
