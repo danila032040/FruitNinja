5111bee08b204a9a8daa7ddf3e33b5e22fa815ba
Даниил Радченко <37844948+danila032040@users.noreply.github.com>
2020-12-10 20:35:41 +0300

    Global gravity value commit
    Added global gravity to PhysicalObject.
    Also Block now inherits PhysicalObject.

--- Assets/Scripts/Models/Block.cs
+++ Assets/Scripts/Models/Block.cs
@@ -1,16 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Block : MonoBehaviour
+﻿public class Block : PhysicalObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     private void OnBecameInvisible()
     {
         Destroy(gameObject);
--- Assets/Scripts/Models/PhysicalObject.cs
+++ Assets/Scripts/Models/PhysicalObject.cs
@@ -2,9 +2,8 @@
 
 public class PhysicalObject : MonoBehaviour
 {
-
     [SerializeField]
-    private float gravity = 9.807f;
+    private float gravityScale = 1f;
     [SerializeField]
     private float weight = 1f;
 
@@ -12,7 +11,7 @@ public class PhysicalObject : MonoBehaviour
     private Vector3 speed;
 
     public Vector3 Acceleration => Force / weight;
-    public Vector3 Force => externalforce + Vector3.down * gravity * weight;
+    public Vector3 Force => externalforce + Physics.gravity * gravityScale * weight;
     public Vector3 Speed => speed;
 
     void Update()
@@ -26,14 +25,23 @@ public class PhysicalObject : MonoBehaviour
         speed += Acceleration * Time.deltaTime;
     }
 
-    public void AddForce(Vector3 force, ForceMode mode = ForceMode.Force)
+    public void AddAcceleration(Vector3 acceleration)
     {
-        switch(mode)
-        {
-            case ForceMode.Acceleration: this.externalforce += force * weight; break;
-            case ForceMode.Force: this.externalforce += force;  break;
-            case ForceMode.Impulse: this.speed += force / weight; break;
-            case ForceMode.VelocityChange: this.speed += force; break;
-        }
+        this.externalforce += acceleration * weight;
+    }
+
+    public void AddForce(Vector3 force)
+    {
+        this.externalforce += force;
+    }
+
+    public void AddImpulse(Vector3 impulse)
+    {
+        this.speed += impulse / weight; 
+    }
+
+    public void AddVelocity(Vector3 velocity)
+    {
+        this.speed += velocity; 
     }
 }
