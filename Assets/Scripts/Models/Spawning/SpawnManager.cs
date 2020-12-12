683d4af24a18eb82cd54d3df94fe706fe89ba21b
Даниил Радченко <37844948+danila032040@users.noreply.github.com>
2020-12-13 00:03:52 +0300

    Added abstraction to SpawnManager commit
    Added abstraction to SpawnManager. Also changed BlockSpawner, now he spawns blocks from random point on line, wich you can describe by 2 points of viewport.

---
+++ Assets/Scripts/Models/Spawning/SpawnManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private List<ZoneSpawner> zoneSpawners = new List<ZoneSpawner>();
+    [SerializeField] private List<Block> blockPrefabs = new List<Block>();
+
+    [SerializeField] private int minBlocksInPack = 2;
+
+    [SerializeField] private int maxBlocksInPack = 5;
+
+    [SerializeField] private float maxDifficulty = 100f;
+
+    [SerializeField] private float minIntervalPack = 2.5f;
+    [SerializeField] private float maxIntervalPack = 4f;
+
+    [SerializeField] private float minIntervalBlock = 0.25f;
+    [SerializeField] private float maxIntervalBlock = 0.5f;
+
+    public void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private int currentDifficulty;
+    private IEnumerator Spawn()
+    {
+        currentDifficulty = 0;
+        while (true)
+        {
+            StartCoroutine(SpawnPackOfBlocks((int)LerpByDifficulty(minBlocksInPack, maxBlocksInPack)));
+            yield return new WaitForSeconds(LerpByDifficulty(maxIntervalPack, minIntervalPack));
+            ++currentDifficulty;
+        }
+    }
+
+    private float LerpByDifficulty(float a, float b) => Mathf.Lerp(a, b, GetPercentage(currentDifficulty, maxDifficulty));
+
+    private float GetPercentage(float min, float max) => Mathf.Min(1, min / max);
+
+    private IEnumerator SpawnPackOfBlocks(int countBlocks)
+    {
+        while (countBlocks > 0)
+        {
+            ChooseZoneSpawnerByPriority().SpawnBlock(GetRandomBlock());
+            --countBlocks;
+            yield return new WaitForSeconds(LerpByDifficulty(maxIntervalBlock, minIntervalBlock));
+        }
+    }
+
+    private ZoneSpawner ChooseZoneSpawnerByPriority()
+    {
+        int summaryPriority = zoneSpawners.Sum(a => a.Priority);
+        int random = Random.Range(0, summaryPriority);
+
+        int currentSum = 0;
+        for (int i=0; i<zoneSpawners.Count; ++i)
+        {
+            if (currentSum < random && random < currentSum+zoneSpawners[i].Priority)
+            {
+                return zoneSpawners[i];
+            }
+            currentSum += zoneSpawners[i].Priority;
+        }
+        return zoneSpawners[0];
+    }
+
+    private Block GetRandomBlock() => blockPrefabs[Random.Range(0, blockPrefabs.Count - 1)];
+
+}
--- Assets/Scripts/Utils/BlockSpawnerPriorityComparerDesc.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-public class BlockSpawnerPriorityComparerDesc : IComparer<BlockSpawner>
-{
-    public int Compare(BlockSpawner x, BlockSpawner y)
-    {
-        return -x.Priority.CompareTo(y.Priority);
-    }
-}
